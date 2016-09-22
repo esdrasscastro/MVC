c5a4bc3a6e30f2ae96bb3c50bc6ac704227477f0
Esdras Castro <esdarscastro@users.noreply.github.com>
2016-09-22 17:21:06 -0300

    Atualizacao para o novo sistema de arquivo, extends Users e alteracao dos metodos resetAttempts e removeAttempts

--- App/Modelo/Login.php
+++ App/Modelo/Login.php
@@ -7,197 +7,26 @@
 
 namespace Modelo;
 
-use Lib\Connection;
-
-class Login
+class Login extends Users
 {
-    /** @var  int */
-    private $userid;
-    /** @var  string */
-    private $username;
-    /** @var  string */
-    private $password;
-    /** @var  string */
-    private $hash;
-    /** @var  int */
-    private $attempts;
-    /** @var  string */
-    private $privilege;
-    /** @var  string */
-    private $last_login;
-    /** @var  boolean */
-    private $published;
-
-    public $tablename = '';
-    private $results = null;
-    private $rowcount = 0;
-
-    /**
-     * @return int
-     */
-    public function getUserid()
-    {
-        return $this->userid;
-    }
-
-    /**
-     * @param int $userid
-     */
-    public function setUserid($userid)
-    {
-        $this->userid = $userid;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * @param string $username
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * @param string $password
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
-
-    /**
-     * @return string
-     */
-    public function getHash()
-    {
-        return $this->hash;
-    }
-
-    /**
-     * @param string $hash
-     */
-    public function setHash($hash)
-    {
-        $this->hash = $hash;
-    }
-
-    /**
-     * @return int
-     */
-    public function getAttempts()
-    {
-        return $this->attempts;
-    }
-
-    /**
-     * @param int $attempts
-     */
-    public function setAttempts($attempts)
-    {
-        $this->attempts = $attempts;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPrivilege()
-    {
-        return $this->privilege;
-    }
-
-    /**
-     * @param string $privilege
-     */
-    public function setPrivilege($privilege)
-    {
-        $this->privilege = $privilege;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLastLogin()
-    {
-        return $this->last_login;
-    }
-
-    /**
-     * @param string $last_login
-     */
-    public function setLastLogin($last_login)
-    {
-        $this->last_login = $last_login;
-    }
-
-    /**
-     * @return boolean
-     */
-    public function isPublished()
-    {
-        return $this->published;
-    }
-
-    /**
-     * @param boolean $published
-     */
-    public function setPublished($published)
-    {
-        $this->published = $published;
-    }
-
     public function resetAttempts($username)
     {
-        return Connection::update($this->tablename, array('attempts'=>5), 'username=:uname', array(':uname'=>$username));
-    }
-
-    public function removeAttempts($username, $amount)
-    {
-        $rs = self::getUser($username);
-        if($rs) {
-            if($rs->rowCount() > 0) {
-                $user = $rs->results();
-
-                $newamount = $user->getAttempts() - $amount;
-                return Connection::update($this->tablename, array('attempts' => $newamount), 'username=:uname', array(':uname' => $username));
-            }
+        $Users = parent::pegar('users_username=:uname', array(':uname'=>$username),'','*',1);
+        if($Users->rowCount()){
+            return $Users->results()->setUsersAttempts(5)->editar();
         }
 
         return false;
     }
 
-    public function getUser($username)
+    public function subtractAttempts($username)
     {
-        $rs = Connection::select($this->tablename, 'username=:uname', array(':uname'=>$username),'','*', '\\Modelo\\Login');
-        if($rs){
-            $this->results = $rs->results;
-            $this->rowcount = $rs->rowCount;
+        $Users = parent::pegar('users_username=:uname', array(':uname'=>$username),'','*',1);
+        if($Users->rowCount()){
+            $Users = $Users->results();
+            return $Users->setUsersAttempts($Users->getUsersAttempts()-1)->editar();
         }
 
-        return $this;
-    }
-
-    public function results()
-    {
-        return $this->results;
-    }
-
-    public function rowCount()
-    {
-        return $this->rowcount;
+        return false;
     }
 }
