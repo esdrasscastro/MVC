6bc49578ff6f4972a5c3b7b8b7d988da3296728b
Esdras Castro <esdarscastro@users.noreply.github.com>
2016-09-22 11:04:14 -0300

    Inclusão do validados de CNPJ e CPF

--- App/Lib/Sistema.php
+++ App/Lib/Sistema.php
@@ -8,6 +8,7 @@
 namespace Lib;
 
 use BadFunctionCallException;
+use Controle\Error;
 use Controle\Login;
 
 class Sistema extends Config
@@ -380,4 +381,75 @@ class Sistema extends Config
 
         require_once parent::$publicoPath.'footer.php';
     }
+
+    /**
+     * Verifica se um número de CPF é válido
+     *
+     * @param string $cpf
+     * @return bool
+     */
+    protected function validaCpf($cpf='')
+    {
+        $cpf = filter_var($cpf, FILTER_SANITIZE_STRING);
+        $cpf = preg_replace("/[^0-9]/", "", $cpf);
+        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+        $invalid = array(00000000000,11111111111,22222222222,44444444444,55555555555,66666666666,77777777777,88888888888,99999999999);
+
+        if(!empty($cpf)){
+            /* verifica se CPF tem 11 números */
+            if(strlen($cpf) != 11) return false;
+            else{
+                if(in_array($cpf, $invalid)) return false;
+                else{
+                    /* Valida o número de CPF */
+                    for ($t = 9; $t < 11; $t++) {
+                        for ($d = 0, $c = 0; $c < $t; $c++) {
+                            $d += $cpf{$c} * (($t + 1) - $c);
+                        }
+                        $d = ((10 * $d) % 11) % 10;
+                        if ($cpf{$c} != $d) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Verifica se um número CPNJ é válido
+     *
+     * @param $cnpj
+     * @return bool
+     */
+    protected function validaCnpj($cnpj='')
+    {
+        $cnpj = preg_replace('/[^0-9]/', '', $cnpj);
+        $cpf = str_pad($cnpj, 14, '0', STR_PAD_LEFT);
+        /* Valida tamanho */
+        if (strlen($cnpj) != 14)
+            return false;
+        /* Valida primeiro dígito verificador */
+        for ($i = 0, $j = 5, $soma = 0; $i < 12; $i++)
+        {
+            $soma += $cnpj{$i} * $j;
+            $j = ($j == 2) ? 9 : $j - 1;
+        }
+        $resto = $soma % 11;
+        if ($cnpj{12} != ($resto < 2 ? 0 : 11 - $resto))
+            return false;
+        /* Valida segundo dígito verificador */
+        for ($i = 0, $j = 6, $soma = 0; $i < 13; $i++)
+        {
+            $soma += $cnpj{$i} * $j;
+            $j = ($j == 2) ? 9 : $j - 1;
+        }
+        $resto = $soma % 11;
+        return $cnpj{13} == ($resto < 2 ? 0 : 11 - $resto);
+    }
+
 }
