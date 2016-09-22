0f096153d61e5c44d21f8b19100fa7124f030ac4
Esdras Castro <esdarscastro@users.noreply.github.com>
2016-09-22 20:04:37 -0300

    Criacao do controlador de pagina do prestador autenticado

---
+++ App/Controle/Painel.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by Grupo B+M
+ * User: Esdras Castro
+ * Date: 22/09/2016
+ */
+
+namespace Controle;
+
+
+use Lib\Sistema;
+
+class Painel extends Sistema
+{
+    public function index()
+    {
+        parent::setPrivilege('prestador');
+
+        self::myPrivilege();
+        parent::setJsScript("Cadastrar.init();");
+        parent::setJsScript("Global.vars.basePath = '".self::$basePath."';");
+        parent::header('Cadastro de Prestador');
+        require_once(self::$htmlPath."cadastro/index.phtml");
+        parent::footer();
+    }
+
+    /**
+     * Verifica se a página precisa ser autenticada
+     *
+     * @return bool
+     */
+    protected static function hasAuth()
+    {
+        return true;
+    }
+
+    /*
+     * Privilégios de acesso a essa página
+     */
+    private function myPrivilege()
+    {
+        if(!parent::userHasPrivilege()){
+            new Error(505);
+            exit;
+        }
+    }
+}
